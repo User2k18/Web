1002796d8cc7e1577cb0af5c92a20b9730b19869
John <86984081+User2k18@users.noreply.github.com>
2022-07-22 00:07:24 -0400

    Create gen.int.php

---
+++ admin/gen.int.php
@@ -0,0 +1,55 @@
+function create_key($pattern){
+    $key = ""; $alphabet = "abcdefghijklmnopqrstuvwxyz";
+    if ($pattern = "") 
+    {
+        return join('-', str_split(substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 16), 4)); }
+    }
+
+    // info
+    //     1 integer
+    //     2 alphabet lower
+    //     3 alphaber upper
+    //     4 alphabet random
+    //- or 5 dash "-"
+    //  other than that go straight example
+    //      
+    // pattern = "111222333444555" => 
+    //           "283htgUIXKhc---"
+    //           "326rngQVChVa---"
+    //
+    // pattern = "Tester4444-111"
+    //            TesterDMWf-614
+    //            TesterEayv-811
+
+            
+    //parsing and generating
+    //i tryed for loop but its keeps breaking my code :(
+    //i wrote parser because i didnt find custom one
+    while ($x != strlen($pattern)){
+
+        switch ($pattern[$x]) {
+            case '1':
+                $key .=  random_int(0,9);
+                break;
+            case '2':
+                $key .=  $alphabet[random_int(0,25)];
+                break;
+            case '3':
+                $key .=  strtoupper($alphabet[random_int(0,25)]);
+                break;
+            case '4':
+                if(random_int(0,1)==1){
+                    $key .=  $alphabet[random_int(0,25)];
+                }else {
+                    $key .= strtoupper($alphabet[random_int(0,25)]);
+                }
+                break;
+            case '5':
+                $key .= "-";//29
+                break;
+            
+        }
+        $x=$x+1;
+    }
+    return $key;
+}
